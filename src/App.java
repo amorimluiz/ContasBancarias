cb41526731d18b901607c7aed88f18ada7902a73
Luiz Felipe dos Reis Amorim <94490217+amorimluiz@users.noreply.github.com>
2022-04-09 00:57:49 -0300

    Implementação de Lista ao invés de vetor

--- src/App.java
+++ src/App.java
@@ -7,18 +7,18 @@ import java.util.Scanner;
 public class App {
 
     static final String nomeArquivo = "contas-bancarias.txt";   //nome do arquivo de dados
-    static ContaBancaria[] contas;  //cria um vetor de objetos
-    static int quantContas;
+    static Lista contas;  //cria um vetor de objetos
+    static int qtde;
 
-    static ContaBancaria[] carregarDados() throws FileNotFoundException{
+    static Lista carregarDados() throws FileNotFoundException{
         File arquivo = new File(nomeArquivo);   //cria o arquivo
         Scanner leitor = new Scanner(arquivo);  //le o arquivo
 
-        int qtdContas = Integer.parseInt(leitor.nextLine());   //primeira linha do arquivo = quantidade de contas
+        int quant = Integer.parseInt(leitor.nextLine());   //primeira linha do arquivo = quantidade de contas
 
-        ContaBancaria novasContas[] = new ContaBancaria[qtdContas*2];
+        Lista novasContas = new Lista(quant * 2);
 
-        for(int i = 0; i < qtdContas; i++){ //para cada conta
+        for(int i = 0; i < quant; i++){ //para cada conta
 
             String linha = leitor.nextLine();   //le a proxima linha do arquivo
             String dados[] = linha.split(";");  //separa os dados no ";"
@@ -29,47 +29,42 @@ public class App {
 
             ContaBancaria novaConta = new ContaBancaria(numero, cpf, saldo); //cria um novo objeto "ContaBancaria"
 
-            novasContas[i] = novaConta; //joga o objeto "ContaBancaria" criado em um vetor de contas.
+            novasContas.inserir(novaConta); //joga o objeto "ContaBancaria" criado em um vetor de contas.
         }
         leitor.close();
-        quantContas = qtdContas;
+        qtde = quant;
         return novasContas;
     }
 
-    static ContaBancaria buscarConta(int numeroConta){
-
-        for(int i = 0; i < quantContas; i++){ //percorre o vetor inteiro
-            if(numeroConta == contas[i].numero)  //se a conta procurada existir
-                    return contas[i];   //retorna a conta
-        }
-        return null;    // se nao existir retorna null
-    }
-
-    static void salvarDados(ContaBancaria[] contas) throws IOException{
+    static void salvarDados(Lista contas) throws IOException{
         File arquivo = new File(nomeArquivo);   //cria o arquivo
         FileWriter escritor = new FileWriter(arquivo, false); //escritor para sobrescrever arquivo
 
-        escritor.append(quantContas + "\n");    //grava na primeira linha do arquivo a quantidade de contas
+        escritor.append(qtde + "\n");    //grava na primeira linha do arquivo a quantidade de contas
 
-        for(int i = 0; i < quantContas; i++)
-            escritor.append(contas[i].numero + ";" + contas[i].cpf + ";" + contas[i].saldo + "\n"); //grava no arquivo o numero da conta, o cpf e o saldo respectivamente. Separados por ;
+        for(int i = 0; i < qtde; i++)
+            escritor.append(contas.contas[i].numero + ";" + contas.contas[i].cpf + ";" + contas.contas[i].saldo + "\n"); //grava no arquivo o numero da conta, o cpf e o saldo respectivamente. Separados por ;
 
         escritor.close();
     }
 
-    static void ordenarContas(ContaBancaria[] contas){
+    static void ordenarContas(Lista contas){
         int posReferencia, posMenor, posAtual;
 
-        for(posReferencia = 0; posReferencia < quantContas - 2; posReferencia++){   //para cada posicao
+        for(posReferencia = 0; posReferencia < qtde; posReferencia++){   //para cada posicao
             posMenor = posReferencia;   //posicao do menor comeca na posicao de referencia
-                for(posAtual = posReferencia+1; posAtual < quantContas - 1; posAtual++){//para cada numero em uma posicao de referencia
-                    if(contas[posAtual].numero < contas[posMenor].numero)    //verifica se o numero da conta atual e menor
+                for(posAtual = posReferencia+1; posAtual < qtde - 1; posAtual++){//para cada numero em uma posicao de referencia
+                    if(contas.contas[posAtual].numero < contas.contas[posMenor].numero)    //verifica se o numero da conta atual e menor
                         posMenor = posAtual; //se sim, posicao menor = posicao atual
                 }
-            ContaBancaria aux = contas[posReferencia];
-            contas[posReferencia] = contas[posMenor]; //realiza a troca do menor valor para a posicao de referencia
-            contas[posMenor] = aux;
+           trocar(contas, posReferencia, posMenor);
         }
+    }
+
+    public static void trocar(Lista contas, int pos1, int pos2){
+        ContaBancaria aux = contas.contas[pos1];
+        contas.contas[pos1] = contas.contas[pos2];
+        contas.contas[pos2] = aux;
     }
 
     static int mostrarMenu(Scanner teclado){
@@ -102,7 +97,7 @@ public class App {
     public static void main(String[] args) throws Exception{
         Scanner teclado = new Scanner(System.in);
         contas = carregarDados();
-        int opcao = 0;
+        int opcao;
 
         do{
             limparTela();
@@ -113,23 +108,24 @@ public class App {
                     limparTela();
                     ContaBancaria contaDesejada;
                     int numero = Integer.parseInt(lerTeclado("Número da conta: ", teclado));    //recebe o numero da conta que deseja buscar
-                    contaDesejada = buscarConta(numero);   //busca a conta desejada e retorna um objeto
+                    contaDesejada = contas.retirar(numero);   //busca a conta desejada e retorna um objeto
 
                     limparTela();
                     if(contaDesejada!=null) //se a conta existir mostra os dados da conta
                         System.out.println(contaDesejada.dadosConta());
                     else    //se nao: conta nao cadastrada
                         System.out.println("Conta não cadastrada");
+                    contas.inserir(contaDesejada);
                     pausar(teclado);
                 break;
 
                 case 2: 
                     limparTela();
-                    int novoNumero = quantContas + 1;
+                    int novoNumero = qtde + 1;
                     long cpf = Long.parseLong(lerTeclado("CPF: ", teclado));    //recebe o cpf da nova conta
                     ContaBancaria novaConta = new ContaBancaria(novoNumero, cpf, 0);    //cria uma nova conta, com o cpf recebido e saldo inicial 0
-                    contas[quantContas] = novaConta;    //joga a nova conta em um vetor de contas                     
-                    quantContas++;
+                    contas.contas[qtde] = novaConta;    //joga a nova conta em um vetor de contas                     
+                    qtde++;
                     System.out.println("Conta cadastrada.");
                     pausar(teclado);
                 break;
@@ -137,8 +133,8 @@ public class App {
                 case 3:
                     limparTela();
                     ordenarContas(contas);  //ordena as contas em ordem crescente
-                    for(int i = 0; i < quantContas; i++)    //para cada conta
-                        System.out.println("Conta número: " + contas[i].numero + " - CPF: " + contas[i].cpf + " - Saldo: " + contas[i].saldo);    //mostra as contas ordenadas
+                    for(int i = 0; i < qtde; i++)    //para cada conta
+                        System.out.println("Conta número: " + contas.contas[i].numero + " - CPF: " + contas.contas[i].cpf + " - Saldo: " + contas.contas[i].saldo);    //mostra as contas ordenadas
                     pausar(teclado);
                 break;
 
